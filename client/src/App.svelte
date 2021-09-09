b666d7f83e73b266f4d9605ee11bb0be1661940f
PixelPerfect <32795849+jw2476@users.noreply.github.com>
2021-09-09 12:25:36 +0000

    Initial commit

---
+++ client/src/App.svelte
@@ -0,0 +1,3 @@
+<main>
+	<h1>HELLO PERSON</h1>
+</main>
---
+++ server/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.listen(process.env.PORT, function () {
+    console.log("Server has started");
+});
